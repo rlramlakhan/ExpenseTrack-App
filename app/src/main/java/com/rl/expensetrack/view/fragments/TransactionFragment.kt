2a9083ea6d3f78fa99e7fd12048740424272ab3b
Ram Lakhan <rl4.ramlakhan@gmail.com>
2025-07-17 14:48:49 +0530

    add home layout

---
+++ app/src/main/java/com/rl/expensetrack/view/fragments/TransactionFragment.kt
@@ -0,0 +1,33 @@
+package com.rl.expensetrack.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rl.expensetrack.R
+import com.rl.expensetrack.databinding.FragmentTransactionBinding
+
+class TransactionFragment : Fragment() {
+    private var _binding: FragmentTransactionBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTransactionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
