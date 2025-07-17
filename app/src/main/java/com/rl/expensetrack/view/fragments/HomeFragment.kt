781b6710bfffffa6bedf237f1c45f8367aa5e4eb
Ram Lakhan <rl4.ramlakhan@gmail.com>
2025-07-17 15:45:04 +0530

    add transaction layout

--- app/src/main/java/com/rl/expensetrack/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/rl/expensetrack/view/fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.rl.expensetrack.R
 import com.rl.expensetrack.databinding.FragmentHomeBinding
 
@@ -23,6 +24,10 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.fabBtnAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_transactionFragment)
+        }
     }
 
     override fun onDestroyView() {
